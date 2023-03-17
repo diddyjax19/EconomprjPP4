ca2ee47778d4db4363b230632d9e1103e2e7c9eb
diddyjax19 <adetobi59@gmail.com>
2023-03-17 06:16:46 +0000

    trying to debug

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%o9!c3rf02q6usr!vw^s96^t*(dsv&ezbs)_u_k7^z1oa$ik0r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nestit.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
