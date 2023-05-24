528a7b973effffea65e5af10a97e745134eb6c56
diddyjax19 <adetobi59@gmail.com>
2023-05-24 22:43:32 +0000

    intial commit of new project

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomprj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phonestore.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
